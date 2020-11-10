45d1abe9d28853b81f9691f9281294e59d69bd8d
HectorLopez1998 <hectorlopezp1998@gmail.com>
2020-11-10 14:02:41 -0500

    commit tree f3d95377d252767336a6defeba8885b009f062e9

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,41 @@
 def create_an_empty_array
-  
+  empty_array = []
 end
 
 def create_an_array
+    empty_array = ["book", "wallet", "phone", "pencil"]
   
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = []
+  return array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = []
+  return array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+    
+    return array.pop
 end
 
 def remove_element_from_start_of_array(array)
   
+  return array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
   
+  return array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  return array.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+  return array.last
 end
